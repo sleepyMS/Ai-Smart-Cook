2ca61a5bd7b1793874da525422b99868befaf2bb
KJHHHHHHH123 <kjhgfd6631@gmail.com>
2024-03-27 11:52:31 +0900

    0327 오전 합치는작업

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -1,15 +1,92 @@
-import React from 'react'
+import React from 'react';
+import "../내가만든css/media.css";
+import "../내가만든css/style.css";
+import { useNavigate } from 'react-router-dom';
+
 
 const Board = () => {
+  const navigate = useNavigate(); 
+  function onSubmitRegister() {
+    navigate('/inboard'); 
+  }
+
   return (
-    <div>
+    <div className="board_wrap">
+      <div className="board_title">
         <h1 className='header_logo'>
-                <a href="/">
-                    <span>MOTIV123131dsadsa1</span>
-                </a>
+            <a href="/">
+                <span>MOTIV</span>
+            </a>
         </h1>
+        <div className="UploadButton">
+        <a href="#" className="on">등록</a>
+          <a href="#">수정</a>
+      </div>
+      <div className="board_list_wrap">
+        <div className="board_list">
+          {/* 각 게시글 항목 */}
+          <div className="top">
+            <div className='num'>번호</div>
+            <div className='title'>제목</div>
+            <div className='writer'>글쓴이</div> {/* 닫는 태그가 누락됨 */}
+            <div className='date'>작성일</div>
+            <div className='count'>조회</div>
+          </div>
+          
+          {/* 실제 게시글 데이터 */}
+          <div>
+            <div className='num'>5</div>
+            <div className='title' onClick={onSubmitRegister}>글 제목</div>
+            <div className='writer'>아무개</div>
+            <div className='date'>작성일</div>
+            <div className='count'>조회</div>
+          </div>
+          <div>
+            <div className='num'>4</div>
+            <div className='title'><a href='InBord.jsx'>글 제목</a></div>
+            <div className='writer'>아무개</div>
+            <div className='date'>작성일</div>
+            <div className='count'>조회</div>
+          </div>
+          <div>
+            <div className='num'>3</div>
+            <div className='title'><a href='InBord.jsx'>글 제목</a></div>
+            <div className='writer'>아무개</div>
+            <div className='date'>작성일</div>
+            <div className='count'>조회</div>
+          </div>
+          <div>
+            <div className='num'>2</div>
+            <div className='title'><a href='InBord.jsx'>글 제목</a></div>
+            <div className='writer'>아무개</div>
+            <div className='date'>작성일</div>
+            <div className='count'>조회</div>
+          </div>
+          <div>
+            <div className='num'>1</div>
+            <div className='title'><a href='InBord.jsx'>글 제목</a></div>
+            <div className='writer'>아무개</div>
+            <div className='date'>작성일</div>
+            <div className='count'>조회</div>
+          </div>
+        </div>
+        <div className="board_page">
+          <a href="#" className="bt first">{"<<"}</a>
+          <a href="#" className="bt prev">{"<"}</a>
+          <a href="#" className="num on">1</a>
+          <a href="#" className="num">2</a>
+          <a href="#" className="num">3</a>
+          <a href="#" className="num">4</a>
+          <a href="#" className="num">5</a>
+          <a href="#" className="bt next">{'>'}</a>
+          <a href="#" className="bt last">'{'>>'}</a>
+        </div>
+        
+          
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Board
+export default Board;
---
+++ src/pages/Write.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import "../내가만든css/media.css"
+import "../내가만든css/style.css"
+
+const Write = () => {
+  return (
+    <div className="board_wrap">
+      <div className="board_title">
+        <strong>공지사항</strong>
+        <p>공지사항을 빠르고 정확하게 안내해드립니다</p>
+      </div>
+      <div className="board_write_wrap">
+        <div className="board_write">
+            <div className='title'></div>
+                <dl>
+                    <dt>제목</dt>
+                    <dd><input type='text' placeholder='제목 입력'></input></dd>
+                    
+                </dl>
+                <dl>
+                    <dt>글쓴이</dt>
+                    <dd><input type='text' placeholder='글쓴이 입력'></input></dd>
+                </dl>
+                <dl>
+                    <dt>비밀번호</dt>
+                    <dd><input type='password' placeholder='비밀번호 입력'></input></dd>
+                </dl>
+            <div className='info'></div>
+            <div className='cont'></div>
+    
+
+        </div>
+            <div className="bt_wrap">
+                <a href='Write.jsx' className='on'>등록</a>
+                <a href='InBord.jsx'>취소</a>
+            </div>
+        </div>
+    </div>
+
+  );
+}
+
+export default Write;
