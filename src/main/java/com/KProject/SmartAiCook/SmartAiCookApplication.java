c3eccb6f3dbcbbd15547ae82e7066870512e04b0
sleepyMS <minseok011017@gmail.com>
2024-03-28 14:42:30 +0900

    reset commit

---
+++ src/main/java/com/KProject/SmartAiCook/SmartAiCookApplication.java
@@ -0,0 +1,13 @@
+package com.KProject.SmartAiCook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAiCookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartAiCookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/KProject/SmartAiCook/SmartAiCookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KProject.SmartAiCook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartAiCookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
