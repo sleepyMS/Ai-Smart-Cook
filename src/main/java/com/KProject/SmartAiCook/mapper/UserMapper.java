33480048967ca73e0e2d133bd875a4352e4bd452
sleepyMS <minseok011017@gmail.com>
2024-03-30 16:24:06 +0900

    Merge remote-tracking branch 'origin/feature/back/Choi' into feature/back/Choi

--- src/main/java/com/KProject/SmartAiCook/mapper/UserMapper.java
+++ src/main/java/com/KProject/SmartAiCook/mapper/UserMapper.java
@@ -6,5 +6,16 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface UserMapper {
 
+    // 새로운 사용자 추가
     public void insertUser(UserDTO userDTO);
+
+    // ID로 사용자 조회
+    public UserDTO getUserById(String id);
+
+    // 사용자 정보 업데이트
+    public void updateUser(UserDTO userDTO);
+
+    // ID로 사용자 삭제
+    public void deleteUser(String id);
+
 }
--- src/test/java/com/KProject/SmartAiCook/MapperTests.java
+++ src/test/java/com/KProject/SmartAiCook/MapperTests.java
@@ -18,12 +18,36 @@ public class MapperTests {
         UserDTO u1 = new UserDTO();
 
         u1.setName("최민석");
-        u1.setId("new");
-        u1.setPhone("010-1234-1234");
+        u1.setId("new3");
+        u1.setPhone("010-3333-3333");
 
         System.out.println(u1);
         userMapper.insertUser(u1);
         System.out.println("------------ 테이블 insert ------------");
+    }
+
+    @Test
+    public void testSelect() {
+        String userId = "new3";
+        UserDTO user = userMapper.getUserById(userId);
+        System.out.println("User found: " + user);
+    }
+
+    @Test
+    public void testUpdate() {
+        String userId = "new2";
+        UserDTO user = userMapper.getUserById(userId);
+        user.setName("update");
+        user.setPhone("010-5555-5555");
+        userMapper.updateUser(user);
+        System.out.println("User updated: " + user);
+    }
+
+    @Test
+    public void testDelete() {
+        String userId = "new3";
+        userMapper.deleteUser(userId);
+        System.out.println("User deleted with ID: " + userId);
     }
 
 }
