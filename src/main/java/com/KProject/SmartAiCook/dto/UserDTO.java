dfdb590a4094b69736467f238665beddbbdb4242
sleepyMS <minseok011017@gmail.com>
2024-03-30 15:18:52 +0900

    Merge remote-tracking branch 'origin/feature/back/Choi' into feature/back/Choi

--- src/main/java/com/KProject/SmartAiCook/dto/UserDTO.java
+++ src/main/java/com/KProject/SmartAiCook/dto/UserDTO.java
@@ -1,49 +1,68 @@
 package com.KProject.SmartAiCook.dto;
 
-public class UserDTO {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class UserDTO {
     private String userName;
     private String userId;
     private String userPhone;
-
-    public UserDTO() {}
-
-    public UserDTO(String userName, String userId, String userPhone) {
-        this.userName = userName;
-        this.userId = userId;
-        this.userPhone = userPhone;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getUserPhone() {
-        return userPhone;
-    }
-
-    public void setUserPhone(String userPhone) {
-        this.userPhone = userPhone;
-    }
-
-    @Override
-    public String toString() {
-        return "UserDTO{" +
-                "userName='" + userName + '\'' +
-                ", userId='" + userId + '\'' +
-                ", userPhone='" + userPhone + '\'' +
-                '}';
-    }
 }
+
+//public class UserDTO {
+//
+//    private String userName;
+//    private String userId;
+//    private String userPhone;
+//
+//    public UserDTO() {}
+//
+//    public UserDTO(String userName, String userId, String userPhone) {
+//        this.userName = userName;
+//        this.userId = userId;
+//        this.userPhone = userPhone;
+//    }
+//
+//    public String getUserName() {
+//        return userName;
+//    }
+//
+//    public void setUserName(String userName) {
+//        this.userName = userName;
+//    }
+//
+//    public String getUserId() {
+//        return userId;
+//    }
+//
+//    public void setUserId(String userId) {
+//        this.userId = userId;
+//    }
+//
+//    public String getUserPhone() {
+//        return userPhone;
+//    }
+//
+//    public void setUserPhone(String userPhone) {
+//        this.userPhone = userPhone;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "UserDTO{" +
+//                "userName='" + userName + '\'' +
+//                ", userId='" + userId + '\'' +
+//                ", userPhone='" + userPhone + '\'' +
+//                '}';
+//    }
+//}
--- src/test/java/com/KProject/SmartAiCook/MapperTests.java
+++ src/test/java/com/KProject/SmartAiCook/MapperTests.java
@@ -1,21 +1,30 @@
 package com.KProject.SmartAiCook;
 
 import com.KProject.SmartAiCook.dto.UserDTO;
+import com.KProject.SmartAiCook.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 public class MapperTests {
+
+    @Autowired
+    private UserMapper userMapper;
 
     @Test
     public void testInsert() {
         UserDTO u1 = new UserDTO();
 
         u1.setUserName("최민석");
-        u1.setUserId("minseok");
-        u1.setUserPhone("010-5848-1017");
+        u1.setUserId("abc");
+        u1.setUserPhone("010-1234-1234");
 
         System.out.println(u1);
+//        userMapper.insertUser(u1);
+//
+//        System.out.println("--------------------------------");
     }
 
 }
